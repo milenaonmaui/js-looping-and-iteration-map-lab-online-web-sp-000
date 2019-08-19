90b14b196c7c3d2deeeff733f7a79d8fdc3973bb
Milena Kari <milenakari@yahoo.com>
2019-08-19 00:20:36 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
 // Code your solution in this file.
+function lowerCaseDrivers(drivers) {
+    return drivers.map(function (driver) {
+      return driver.toLowerCase();
+    });
+}
+
+function nameToAttributes(drivers) {
+  return 
+}
